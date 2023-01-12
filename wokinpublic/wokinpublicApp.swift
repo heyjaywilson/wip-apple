e72706b2d15b2cc9777134a92e7daf5343089792
Jay <heyjay@cctplus.dev>
2023-01-12 17:12:01 -0600

    Initial Commit

---
+++ wokinpublic/wokinpublicApp.swift
@@ -0,0 +1,17 @@
+//
+//  wokinpublicApp.swift
+//  wokinpublic
+//
+//  Created by Jay on 1/12/23.
+//
+
+import SwiftUI
+
+@main
+struct wokinpublicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ wokinpublicUITests/wokinpublicUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  wokinpublicUITestsLaunchTests.swift
+//  wokinpublicUITests
+//
+//  Created by Jay on 1/12/23.
+//
+
+import XCTest
+
+final class wokinpublicUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
