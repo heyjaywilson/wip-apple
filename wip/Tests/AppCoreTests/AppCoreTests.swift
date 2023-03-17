2361990533fbda72f939ce14479a874b5158b32c
Jay <heyjay@cctplus.dev>
2023-03-17 15:48:31 -0500

    test: new project flow

--- wip/Tests/AppCoreTests/AppCoreTests.swift
+++ wip/Tests/AppCoreTests/AppCoreTests.swift
@@ -8,6 +8,7 @@
 import XCTest
 import ComposableArchitecture
 import AppCore
+import NewProjectCore
 import WIPKit
 import API
 
@@ -20,17 +21,6 @@ final class AppCoreTests: XCTestCase {
 
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testAddProjectButtonTapped() async {
-        let store = TestStore(
-            initialState: AppCore.State(),
-            reducer: AppCore()
-        )
-
-        await store.send(.addProjectButtonTapped) {
-            $0.showAddProjectForm = true
-        }
     }
 
     func testOnApperSuccess() async {
@@ -70,6 +60,57 @@ final class AppCoreTests: XCTestCase {
 
         await store.receive(.getProjectsResponse(.failure(APIError()))) {
             $0.apiError = APIError()
+        }
+    }
+
+    func testAddProjectButtonTapped() async {
+        let store = TestStore(
+            initialState: AppCore.State(),
+            reducer: AppCore()
+        )
+
+        await store.send(.addProjectButtonTapped) {
+            $0.showAddProjectForm = true
+        }
+    }
+
+    func testNewProjectCancelTapped() async {
+        let store = TestStore(
+            initialState: AppCore.State(),
+            reducer: AppCore()
+        )
+        await store.send(.addProjectButtonTapped) {
+            $0.showAddProjectForm = true
+        }
+        await store.send(.newProject(.cancelButtonTapped))
+        await store.receive(.newProject(.delegate(.cancelButtonTapped))) {
+            $0.showAddProjectForm = false
+        }
+    }
+
+    func testNewProjectSuccessfulResponse() async {
+        let store = TestStore(
+            initialState: AppCore.State(),
+            reducer: AppCore()
+        )
+        await store.send(.addProjectButtonTapped) {
+            $0.showAddProjectForm = true
+        }
+        await store.send(.newProject(.addProjectButtonTapped))
+        await store.receive(
+            .newProject(
+                .createProjectResponse(
+                    .success(
+                        Project(
+                            id: UUID(uuidString: "00000000-0000-0000-0000-000000000000"),
+                            title: "Test Project"
+                        )
+                    )
+                )
+            )
+        )
+        await store.receive(.newProject(.delegate(.successfulResponse))) {
+            $0.showAddProjectForm = false
         }
     }
 
